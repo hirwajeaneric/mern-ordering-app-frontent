3580b7908768d3b2b445915654ac742dc0956178
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-27 16:22:24 +0200

    Feature: Manage Restaurant page - Details section

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoutes from "./auth/ProtectedRoutes";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AppRoutes = () => {
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route element={<ProtectedRoutes />}>
         <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+        <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage /></Layout>} />
       </Route>
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
